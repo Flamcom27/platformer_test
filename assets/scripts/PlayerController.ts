c5f83e76e4b41ee33f5d87345e33fd24a4386945
unknown <27flamcom06@gmail.com>
2025-03-17 06:22:18 +0300

    PlayerController is refactored

--- assets/scripts/PlayerController.ts
+++ assets/scripts/PlayerController.ts
@@ -3,53 +3,37 @@ const { ccclass, property } = _decorator;
 
 @ccclass('PlayerController')
 export class PlayerController extends Component {
+
     private _startJump: boolean = false;
-    private _jumpStep: number = 0;
     private _curPos: Vec3 = new Vec3();
     private _targetPos: Vec3 = new Vec3();
-    private _curJumpSpeed: number = 0;
-    private _deltaPos: Vec3 = new Vec3(0, 0, 0);
-    private _curJumpTime: number = 0;
-    private _jumpTime: number = 1;
+    private _jumpTime: number = 0.5;
 
     start() {
         input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
     }
     onMouseUp(event: EventMouse) {
-        if (event.getButton() === 0) {
-            this.jumpByStep(1);
-        }
-        else if (event.getButton() === 2) {
-            this.jumpByStep(2);
+        let button = event.getButton()
+        if (button === 0 || button === 2 && !this._startJump) {
+            this.jumpByStep( {0: 1, 2: 2}[button] );
         }
     }
     jumpByStep(step: number) {
-        if (this._startJump) {
-            return;
-        }
         this._startJump = true;
-        this._jumpStep = step;
-        this._curJumpTime = 0;
         this.node.getPosition(this._curPos);
-
-        Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep, 0, 0));
+        Vec3.add(this._targetPos, this._curPos, new Vec3(step, 0, 0));
+        tween(this.node)
+            .to(this._jumpTime, { position: this._targetPos }, {
+                easing: "linear",
+                onComplete: () => {
+                    this._startJump = false
+                }
+            })
+            .start()
     }
-    update(deltaTime: number) {
-        if (this._startJump) { 
-            this._curJumpTime += deltaTime;  
-            if (this._curJumpTime > this._jumpTime) { 
-                this.node.setPosition(this._targetPos); 
-                this._startJump = false
-            } else {
-                tween(this.node)
-                    .to(this._jumpTime, { position: this._targetPos }, { easing: "linear" })
-                    .start()
-            }
-        }
+    //update(deltaTime: number) {
         
-    }
-
-    
+    //}    
 }
 
 
